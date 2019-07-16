e1dbdb0058370d06122ff45a36fb7a1bba658ec9
Hack Reactor Student <student@hackreactor.com>
2019-07-15 17:55:03 -0700

    6th try

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,81 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  return json(obj);
 };
+
+var json = function(obj) {
+  // Base cases-primitive types
+  // Check for unstringifiablevalues i.e, function(){}, undefined
+  // number
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+  // null
+  if (obj === null) {
+    return 'null';
+  }
+    
+  // true or false
+  if (typeof obj === 'boolean') {
+    if (obj === true) {
+      return 'true';
+    } else {
+      return 'false';
+    }
+  }
+    
+  // string
+  if (typeof obj === 'string') {
+    return '\"' + obj + '\"';
+  }
+  
+  // Recursive cases
+  // Arrays 
+  if (Array.isArray(obj) === true) {
+  // Empty Array
+    if (obj.length === 0 ) {
+      return '[]';
+    } else {
+      return '[' + stringifyArray(obj) + ']';
+    }
+  } 
+  // Objects
+  if (typeof obj === 'object') {
+    if (Object.keys(obj).length > 0) {
+      return stringifyObj(obj);
+    } else {
+      return '{}';
+    }
+  }
+};
+
+var stringifyArray = function(obj) {
+  var newArray = [];
+  /*var newArray = "";
+  
+  if (obj.length >1) {
+    for (var i=0; i<obj.length -1; i++){
+      newArray = newArray + json(obj[i]) + "," 
+    }
+  }
+ return newArray + json(obj[obj.length-1])*/
+ 
+  if (obj.length > 0) {
+    for (var i = 0; i < obj.length; i++) {
+      newArray.push(json(obj[i]));
+    }
+  }
+  return newArray;
+};
+
+var stringifyObj = function(obj) {
+  var newObj = '{';
+  for (var key in obj) {
+    newObj = newObj + '"' + key + '":' + json(obj[key]) + ',';  
+  }
+  newObj = newObj.substring(0, newObj.length - 1);
+  return newObj + '}';
+};
+
+
